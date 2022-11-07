755ef053aaf4bd9f37010d18ae93b4cbc15fc91d
Vladimir <zaicev.94@mail.ru>
2022-11-07 18:41:31 +0300

    src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 7000 ;
+        int bonusprice = 20 ;
+
+        int bonus = ticketprice / bonusprice;
+        System.out.println("Your bonus is:"+ bonus +" miles");
+
+    }
+}
