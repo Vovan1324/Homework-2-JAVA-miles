561e94d5a07ce2522b5621406cbaec3c18a86de1
Vladimir <zaicev.94@mail.ru>
2022-11-09 20:07:03 +0300

    change .idea/workspace.xml

---
+++ src/BonusMilesServise.java
@@ -0,0 +1,10 @@
+public class BonusMilesServise {
+
+    public int calculate (int cost) {
+        int MilePerCost = 20;
+        int miles = cost / MilePerCost;
+
+                return miles;
+    }
+
+}
